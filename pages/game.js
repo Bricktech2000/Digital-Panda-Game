614c486be97eeca63dbf57854f5e5a5ab86d5af6
Hobomojo38 <Hobomojo3@gmail.com>
2021-11-09 01:12:23 -0500

    Added price and total deduction to game.js

--- pages/game.js
+++ pages/game.js
@@ -5,23 +5,37 @@ import styles from '../styles/index.module.css';
 var clickMult = 1
 
 const Game = () => {
-  const [count, setCount] = useState(0);
+  const [cookieTotal, setCookieTotal] = useState(0);
   const [grandmaCount, setGrandmaCount] = useState(0);
-  const [grandmaPrice, setGrandmaPrice] = useState(0);
+  const [grandmaPrice, setGrandmaPrice] = useState(10);
   
+  /**
+   * Returns True if price is under the global value of cookieTotal
+   * @param {*} price 
+   * @returns bool
+   */
+  const checkUnderPrice = (price) => {
+    return price <= cookieTotal 
+  }
 
   const grandmaUpdate = () => {
-    clickMult += 0.1
-    setGrandmaCount(grandmaCount + 1)
-    setGrandmaPrice(grandmaPrice + 1)
-    console.log(clickMult)
+    if (checkUnderPrice(grandmaPrice) == true) {
+      clickMult += 0.1
+      setGrandmaCount(grandmaCount + 1)
+
+      setGrandmaPrice(grandmaPrice + 1)
+      setCookieTotal(cookieTotal - grandmaPrice)
+
+      console.log(clickMult)
+    }
+    
   }
 
   return (
     <div className={styles.Index}>
-      <Button onClick={() => {setCount((count + 1*clickMult))}}>{Math.floor(count)}</Button>
+      <Button onClick={() => {setCookieTotal((cookieTotal + 1*clickMult))}}>{Math.floor(cookieTotal)}</Button>
       <h1 >Current Grandma Count: {grandmaCount}</h1>
-      <Button onClick = {grandmaUpdate}>Buy Grandma</Button>
+      <Button onClick = {grandmaUpdate}>Buy Grandma (${grandmaPrice})</Button>
     </div>
     
     );
